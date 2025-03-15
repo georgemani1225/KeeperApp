1ccc40eedc27d02257964c55f081b66106091152
georgemani1225 <georgemani1225@gmail.com>
2025-03-15 17:04:13 +0530

    Deleted files

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import noteRoutes from './routes/note.route.js';
 import cors from 'cors';
-import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -13,14 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/notes", noteRoutes)
 
-
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-  });
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:" + PORT);
 });
