b1c5dffa2914e1e72b0f8ae86ea1e9afa6ae1224
georgemani1225 <georgemani1225@gmail.com>
2025-03-16 16:48:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import noteRoutes from './routes/note.route.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -11,11 +12,11 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/notes", noteRoutes)
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/", (req, res) => {
-    res.send("Server is running!");
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
-
 
 app.listen(PORT, () => {
     connectDB();
