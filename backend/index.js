ebc694c771ff7b5a73649406fc9835d508ff1ff1
georgemani1225 <georgemani1225@gmail.com>
2025-03-16 16:52:20 +0530

    Updates

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/notes", noteRoutes)
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
