4ae6cb2579d664e47effbec817e6c71c437ab1b8
georgemani1225 <georgemani1225@gmail.com>
2025-03-22 18:06:26 +0530

    Modified Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,35 @@ import React from "react";
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login-box">
+      <div className="login-header">
+        <header>Login</header>
+      </div>
+      <div className="input-box">
+        <input type="text" className="input-field" placeholder="Email" autoComplete="off" required />
+      </div>
+      <div className="input-box">
+        <input type="password" className="input-field" placeholder="Password"autoComplete="off" required />
+      </div>
+      <div className="forgot">
+        <section>
+          <a href="#">Forgot password</a>
+        </section>
+      </div>
+      <div className="input-submit">
+        <button className="submit-btn" id="submit">
+          Sign In
+        </button>
+      </div>
+      <div className="sign-up-link">
+        <p>
+          Don't have an account? <a href="#">Sign Up</a>
+        </p>
+      </div>
+    </div>
   );
 }
+
+
 
 export default Login;
